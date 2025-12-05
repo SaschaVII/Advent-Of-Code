8f1a727513fa4a3c98ee9bef45fdc3c0affcc7dc
kingdomoVuigeil <domo.kos@gmx.at>
2025-12-05 17:07:40 +0100

    cleared prints

--- Domo/Dec5.py
+++ Domo/Dec5.py
@@ -1,4 +1,3 @@
-
 if __name__ == '__main__':
 
     file_path = 'inputTest.txt'
@@ -8,7 +7,6 @@ if __name__ == '__main__':
         lines = file.readlines()
 
     list_of_ranges = []
-    list_of_numbers = []
     total = 0
 
     for raw in lines:
@@ -24,24 +22,11 @@ if __name__ == '__main__':
 
     sorted_ranges = sorted(list_of_ranges, key = lambda number: number)
 
-    print("sorted:",sorted_ranges)
-
     for i in range(len(sorted_ranges)-1):
-
         if sorted_ranges[i][1] >= sorted_ranges[i+1][0]:
             if (sorted_ranges[i][1] > sorted_ranges[i+1][1]):
-                print("fuck")
-                print(sorted_ranges[i])
-                print(sorted_ranges[i+1])
-                print("---")
                 sorted_ranges[i+1][1] = sorted_ranges[i][1]
-
             sorted_ranges[i][1] = sorted_ranges[i+1][0] -1
-
         total += sorted_ranges[i][1] - sorted_ranges[i][0] + 1
     total += sorted_ranges[len(sorted_ranges)-1][1] - sorted_ranges[len(sorted_ranges)-1][0] + 1
-
-    print("-----")
-
-    print("cut:",sorted_ranges)
     print(total)
