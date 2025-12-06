eca354962317a472b647dbba587081424388b08e
kingdomoVuigeil <domo.kos@gmx.at>
2025-12-06 15:18:03 +0100

    removed 66 % of lines

--- Domo/Dec6.py
+++ Domo/Dec6.py
@@ -1,3 +1,1 @@
-from math import prod; from itertools import groupby
-l=open('inputTest.txt').read().splitlines()
-print(sum((sum(a) if b=='+' else prod(a)) for b,a in zip(l.pop().split(), [[int(''.join(c)) for c in g] for k,g in groupby(zip(*l), lambda c: set(c)=={' '}) if not k])))
+import math,itertools;l=open('inputTest.txt').read().splitlines();print(sum(sum(a)if b=='+'else math.prod(a)for b,a in zip(l.pop().split(),[[int(''.join(c))for c in g]for k,g in itertools.groupby(zip(*l),lambda c:set(c)=={' '})if not k])))
