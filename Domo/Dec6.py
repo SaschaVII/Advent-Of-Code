4a8e6aef30fb34b60cc6824851d4dba103a44cb3
kingdomoVuigeil <domo.kos@gmx.at>
2025-12-07 18:38:45 +0100

    Day 7 unfinished

--- Domo/Dec6.py
+++ Domo/Dec6.py
@@ -1,1 +1,80 @@
-import math,itertools;l=open('inputTest.txt').read().splitlines();print(sum(sum(a)if b=='+'else math.prod(a)for b,a in zip(l.pop().split(),[[int(''.join(c))for c in g]for k,g in itertools.groupby(zip(*l),lambda c:set(c)=={' '})if not k])))
+import numpy as np
+
+def getMult(list_of_numbers):
+    total = 1
+    for number in list_of_numbers:
+        total *= number
+    return total
+
+def getPlus(list_of_numbers):
+    total = 0
+    for number in list_of_numbers:
+        total += number
+    return total
+
+def getLineTotal(list_of_numbers, operator):
+    if operator == "+":
+        return getPlus(list_of_numbers)
+    return getMult(list_of_numbers)
+
+
+if __name__ == '__main__':
+
+    file_path = 'inputTest.txt'
+
+    with open(file_path, 'r') as file:
+
+        lines = file.readlines()
+
+    list_of_columns = []
+    total = 0
+    transposed_columns = []
+    operator_line = []
+#Part 1:
+    for raw in lines:
+        line = raw.strip()
+        if not line:
+            continue
+        
+        
+        # Split by minus to get individual pairs
+        parts = line.split()
+        current_line = []
+        for part in parts:
+
+            if "*" in part or "+" in part:
+                operator_line.append(part)
+            else:
+                current_line.append(int(part))
+        if current_line:
+            list_of_columns.append(current_line)
+
+    print("lines:  ",len(lines))
+    print("columns:  ",len(lines[0]))
+
+    numbers_tuple = []
+
+    all_tuples = []
+
+    for col in range(len(lines[0])-1):
+        number_str = []
+        for row in range(len(lines)-1):
+            number_str.append(lines[row][col])
+        
+        whitespaces_amount = number_str.count(" ")
+        if (whitespaces_amount < 4):
+            numbers_tuple.append(int(''.join(number_str)))
+        else:
+            all_tuples.append(numbers_tuple)
+            numbers_tuple = []
+    all_tuples.append(numbers_tuple)
+        
+    transposed_columns = np.transpose(list_of_columns)
+    i = 0
+    for operator in operator_line:
+        total += getLineTotal(all_tuples[i],operator)
+        i+=1
+
+    # print(transposed_columns)
+    # print(operator_line)
+    print(total)
---
+++ Domo/Dec7.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+
+EMPTY_SPACE = 0
+SPLITTER = 1
+BEAM = 2
+STARTING_POINT = 3
+
+
+if __name__ == '__main__':
+
+    file_path = 'inputTest.txt'
+
+    integer_map = []
+
+
+    with open(file_path, 'r') as file:
+
+        lines = file.readlines()
+        for line in lines:
+            integer_line = []
+            for char in line:
+                match char:
+                    case ".":
+                        integer_line.append(EMPTY_SPACE)
+
+                    case "^":
+                        integer_line.append(SPLITTER)
+
+                    case "S":
+                        integer_line.append(STARTING_POINT)
+            integer_map.append(integer_line)
+        print (integer_map)
+
+    beam_strength_map = np.zeros((len(integer_map),len(integer_map[0])))
+
+  
+#Part 1:
+    splits_total = 0
+    for row in range(1,len(integer_map)):
+        for col in range (len(integer_map[0])-1):
+            print ("Row: ", row, "Column: ", col)
+            char_on_top = integer_map[row-1][col]
+            char_left = integer_map[row][col-1]
+            char_right = integer_map[row][col+1]
+            char_current = integer_map[row][col]
+
+            beam_strength_top = beam_strength_map[row-1][col]
+
+            if char_on_top != BEAM and char_on_top != STARTING_POINT:
+                continue
+            if char_current == EMPTY_SPACE:
+                integer_map[row][col] = BEAM
+
+                if char_on_top == STARTING_POINT:
+                    beam_strength_map[row][col] = 1
+                else:    
+                    beam_strength_map[row][col] = beam_strength_top
+                
+                continue
+            if char_current == SPLITTER:
+                if col > 0:
+                    integer_map[row][col-1] = BEAM
+                    beam_strength_map[row][col-1] += beam_strength_top
+                if col < len(integer_map[0]):
+                    integer_map[row][col+1] = BEAM
+                    beam_strength_map[row][col+1] += beam_strength_top
+                splits_total +=1
+    
+    print (integer_map)
+    print("splits total ", splits_total)
+    print (beam_strength_map)
+    print("total Beams:",sum(beam_strength_map[len(integer_map)-2]))
+            
+    
+
+
+    
