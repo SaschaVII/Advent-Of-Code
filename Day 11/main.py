6401f1aad4009663e85e415851424a9cf2628ccf
Sascha Kuhness <sascha.kuhness.acuadev@outlook.com>
2025-12-19 09:26:45 +0000

    day 11 part 2, wow that actually was a breeze lol

--- Day 11/main.py
+++ Day 11/main.py
@@ -28,6 +28,29 @@ def ways_to_end(name: str, graph: dict[str, list[str]]) -> int:
 
     return count
 
+memo = {} # memo needs to include the fft and dac states
+def ways_to_end_2(name: str, graph: dict[str, list[str]], fft: bool = False, dac: bool = False) -> int:
+    key = f"{name} {fft} {dac}"
+    if key in memo.keys():
+        return memo[key]
+
+    fft = True if name == "fft" else fft
+    dac = True if name == "dac" else dac
+    
+    if "out" in graph[name]:
+        if (fft and dac):
+            memo[key] = 1
+            return 1
+        memo[key] = 0
+        return 0
+    
+    count = 0
+    for output in graph[name]:
+        count += ways_to_end_2(output, graph, fft, dac)
+
+    memo[key] = count
+    return count
+
 
 def part_1():
     graph = get_graph()
@@ -36,11 +59,16 @@ def part_1():
 
     print(f"Result part 1: {result}")
         
+# all paths that go from "svr" through "fft" and "dac" then "out"
+def part_2():
+    graph = get_graph()
+    result = ways_to_end_2("svr", graph)
 
+    print(f"Result part 2: {result}")
 
 
 def main():
-    part_1()
+    part_2()
 
 if __name__ == "__main__":
     main()
